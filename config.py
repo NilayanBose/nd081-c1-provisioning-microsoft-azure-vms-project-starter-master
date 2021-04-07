473fa7bc89adb167195e8dfdd731d5e9767d87ea
Nilayan <nilayanbose14@gmail.com>
2021-04-07 23:39:18 +0530

    add

--- config.py
+++ config.py
@@ -5,14 +5,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key'
 
-    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'ENTER_STORAGE_ACCOUNT_NAME'
-    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'ENTER_BLOB_STORAGE_KEY'
-    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'ENTER_IMAGES_CONTAINER_NAME'
+    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'cmsstorageacc'
+    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'Uvl2FPxemV5NIl9evvjz2o3VYyYjxPCKLW6dRjMQWQuaB42Djql8JFwo2jkyHa1RW+tBY3AHjY+WcvSUjjGsww=='
+    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'images'
 
-    SQL_SERVER = os.environ.get('SQL_SERVER') or 'ENTER_SQL_SERVER_NAME.database.windows.net'
-    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'ENTER_SQL_DB_NAME'
-    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'ENTER_SQL_SERVER_USERNAME'
-    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'ENTER_SQL_SERVER_PASSWORD'
+    SQL_SERVER = os.environ.get('SQL_SERVER') or 'testcmsserver.database.windows.net'
+    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'cmsdb'
+    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'usr'
+    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'welcome@1234'
     # Below URI may need some adjustments for driver version, based on your OS, if running locally
     SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE  + '?driver=ODBC+Driver+17+for+SQL+Server'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -23,14 +23,14 @@ class Config(object):
     # In your production app, Microsoft recommends you to use other ways to store your secret,
     # such as KeyVault, or environment variable as described in Flask's documentation here:
     # https://flask.palletsprojects.com/en/1.1.x/config/#configuring-from-environment-variables
-    CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+    CLIENT_SECRET = os.getenv("CLIENT_SECRET") or 'P4D__vT_62wh0f~.f72G9LwqQFf.Qd~obb'
     if not CLIENT_SECRET:
         raise ValueError("Need to define CLIENT_SECRET environment variable")
 
     AUTHORITY = "https://login.microsoftonline.com/common"  # For multi-tenant app, else put tenant name
     # AUTHORITY = "https://login.microsoftonline.com/9ab75c1d-a904-4862-af8a-d1c7903df9f8"
 
-    CLIENT_ID = os.getenv("CLIENT_ID")
+    CLIENT_ID = os.getenv("CLIENT_ID") or 'a4aaa8d6-74f9-424f-8759-9ecc30388610'
 
     REDIRECT_PATH = "/getAToken"  # Used to form an absolute URL; must match to app's redirect_uri set in AAD
 
